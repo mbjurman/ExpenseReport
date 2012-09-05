1446da3c8987530aef4d24092452b1812d9b38e1
Marcus Bjurman <mbjurman@gmail.com>
2012-09-05 22:49:57 +0200

    Improved ember implementation

--- ExpenseReportWeb/expensereport.js
+++ ExpenseReportWeb/expensereport.js
@@ -1,43 +1,13 @@
-function loadFunc(e) {
-    var ctrl = $(e).parent();
-    account = ctrl.text();
-    ctrl = ctrl.next();
-    ctrl = ctrl.next();
-    var vatOnly = ctrl.text();
-    ctrl = ctrl.next();
-    var totalSum = ctrl.text();
-
-    $('#select-account').val(account);
-    $('#manual-account').val(account);
-    $('#total-sum').val(totalSum);
-    $('#vat-only').val(vatOnly);
-    $('#myModalExpense').modal('show');
-}
-
-function getCommonExpenseObject()
-{
-    var result = new Object();
-    var selectAccount = $('#select-account').val();
-    var manualAccount = $('#manual-account').val();
-    result.account = selectAccount;
-
-    if (manualAccount) {
-        result.account = manualAccount;
-    }
-
-    result.totalSum = parseFloat($('#total-sum').val());
-    result.vatOnly = parseFloat($('#vat-only').val());
-    return result;
-}
 
 function addCommonExpense() 
 {
 //    App.MainPage.Expenses.pushObject(Em.copy(App.EditedExpense, true));
 
     App.MainPage.Expenses.pushObject(
-      Em.Object.create({
-          No: App.EditedExpense.get('No'),
-          Account: App.EditedExpense.get('Account'),
+      App.ExpenseClass.create({
+          Nr: App.EditedExpense.get('No'),
+          SelectedAccount: App.EditedExpense.get('SelectedAccount'),
+          OtherAccount: App.EditedExpense.get('OtherAccount'),
           Cost: App.EditedExpense.get('Cost'),
           VAT: App.EditedExpense.get('VAT'),
           Total: App.EditedExpense.get('Total')
--- ExpenseReportWeb/js/app.js
+++ ExpenseReportWeb/js/app.js
@@ -7,22 +7,64 @@ App.MyView = Em.View.extend({
     }
 });
 
+App.ExpenseClass = Em.Object.extend({
+    Nr: 0,
+    SelectedAccount: null,
+    OtherAccount: "",
+    Cost: "",
+    VAT: "",
+    Total: "",
+    AccountDisplay: function() {
+        if (Ember.empty(this.get('OtherAccount')))
+    		return this.get('SelectedAccount').Name;
+    	else
+    		return this.get('OtherAccount');
+    }.property()
+});
+
+App.Account = Ember.Object.extend({
+    Nr: null,
+    Name: null
+});
+
 App.MainPage = Em.Object.create({
     msg: "test",
     Expenses: [
-        Em.Object.create({
-            No: 1,
-            Account: "blaha",
+        App.ExpenseClass.create({
+            Nr: 1,
+            SelectedAccount: App.Account.create({Nr: 6, Name: "Förbrukningsinventarier" }),
+            OtherAccount: "blaha",
             Cost: 100,
             VAT: 25,
-            Total: 125
-        })],
+            Total: 125,
+        })]
 });
 
-App.EditedExpense = Em.Object.create({
-    No: 0,
-    Account: "",
+App.EditedExpense = App.ExpenseClass.create({
+    Nr: 0,
+    SelectedAccount: null,
+    OtherAccount: "",
     Cost: "",
     VAT: "",
     Total: ""
 });
+
+App.selectedAccountController = Ember.Object.create({
+    account: null
+});
+
+App.accountController = Ember.ArrayController.create({
+    content: [
+		App.Account.create({Nr: 5, Name: "Kontorsmateriel" }),
+		App.Account.create({Nr: 6, Name: "Förbrukningsinventarier" }),
+		App.Account.create({Nr: 7, Name: "Övriga personalkostnader" }),
+		App.Account.create({Nr: 8, Name: "Friskvårdsbidrag" }),
+		App.Account.create({Nr: 9, Name: "Tidningar och fackliteratur" }),
+		App.Account.create({Nr: 10, Name: "Flyg" }),
+		App.Account.create({Nr: 11, Name: "Taxi" }),
+		App.Account.create({Nr: 12, Name: "Övriga resekostnader" }),
+		App.Account.create({Nr: 13, Name: "Kost och logi" }),
+		App.Account.create({Nr: 14, Name: "Faktureras kund" })
+    ]
+});
+
